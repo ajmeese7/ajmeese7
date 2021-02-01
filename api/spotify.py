e546816006a2e571c879721f199e16d08bacf904
Aaron Meese <ajmeese7@gmail.com>
2021-02-01 07:27:37 -0600

    Update Spotify preview
    The mini preview hasn't been working for a few days, so I'm hoping this latest update will kick things back into gear. If not I'm gonna have do get fancy with it, because I don't know what the issue is.

--- api/spotify.py
+++ api/spotify.py
@@ -9,110 +9,126 @@ from flask import Flask, Response, jsonify, render_template
 
 load_dotenv(find_dotenv())
 
-# Spotify:
+# Spotify scopes:
 #   user-read-currently-playing
 #   user-read-recently-played
 SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
 SPOTIFY_SECRET_ID = os.getenv("SPOTIFY_SECRET_ID")
 SPOTIFY_REFRESH_TOKEN = os.getenv("SPOTIFY_REFRESH_TOKEN")
 
-SPOTIFY_URL_REFRESH_TOKEN = "https://accounts.spotify.com/api/token"
-SPOTIFY_URL_NOW_PLAYING = "https://api.spotify.com/v1/me/player/currently-playing"
-SPOTIFY_URL_RECENTLY_PLAY = "https://api.spotify.com/v1/me/player/recently-played?limit=10"
+REFRESH_TOKEN_URL = "https://accounts.spotify.com/api/token"
+NOW_PLAYING_URL = "https://api.spotify.com/v1/me/player/currently-playing"
+RECENTLY_PLAYING_URL = (
+	"https://api.spotify.com/v1/me/player/recently-played?limit=10"
+)
 
 app = Flask(__name__)
 
+
 def getAuth():
-    return b64encode(f"{SPOTIFY_CLIENT_ID}:{SPOTIFY_SECRET_ID}".encode()).decode("ascii")
+	return b64encode(f"{SPOTIFY_CLIENT_ID}:{SPOTIFY_SECRET_ID}".encode()).decode(
+		"ascii"
+	)
+
 
 def refreshToken():
-    data = {
-        "grant_type": "refresh_token",
-        "refresh_token": SPOTIFY_REFRESH_TOKEN,
-    }
+	data = {
+		"grant_type": "refresh_token",
+		"refresh_token": SPOTIFY_REFRESH_TOKEN,
+	}
 
-    headers = {"Authorization": "Basic {}".format(getAuth())}
+	headers = {"Authorization": "Basic {}".format(getAuth())}
+	response = requests.post(REFRESH_TOKEN_URL, data=data, headers=headers)
 
-    response = requests.post(SPOTIFY_URL_REFRESH_TOKEN, data=data, headers=headers)
-    return response.json()["access_token"]
+	try:
+		return response.json()["access_token"]
+	except KeyError:
+		print(json.dumps(response.json()))
+		print("\n---\n")
+		raise KeyError(str(response.json()))
+
 
 def recentlyPlayed():
-    token = refreshToken()
-    headers = {"Authorization": f"Bearer {token}"}
-    response = requests.get(SPOTIFY_URL_RECENTLY_PLAY, headers=headers)
+	token = refreshToken()
+	headers = {"Authorization": f"Bearer {token}"}
+	response = requests.get(RECENTLY_PLAYING_URL, headers=headers)
 
-    if response.status_code == 204:
-        return {}
+	if response.status_code == 204:
+		return {}
+	return response.json()
 
-    return response.json()
 
 def nowPlaying():
-    token = refreshToken()
-    headers = {"Authorization": f"Bearer {token}"}
-    response = requests.get(SPOTIFY_URL_NOW_PLAYING, headers=headers)
+	token = refreshToken()
+	headers = {"Authorization": f"Bearer {token}"}
+	response = requests.get(NOW_PLAYING_URL, headers=headers)
 
-    if response.status_code == 204:
-        return {}
+	if response.status_code == 204:
+		return {}
+	return response.json()
 
-    return response.json()
 
 def barGen(barCount):
-    barCSS = ""
-    left = 1
-    for i in range(1, barCount + 1):
-        anim = random.randint(1000, 1350)
-        barCSS += ".bar:nth-child({})  {{ left: {}px; animation-duration: {}ms; }}".format(
-            i, left, anim
-        )
-        left += 4
+	barCSS = ""
+	left = 1
+	for i in range(1, barCount + 1):
+		anim = random.randint(1000, 1350)
+		barCSS += (
+			".bar:nth-child({})  {{ left: {}px; animation-duration: {}ms; }}".format(
+				i, left, anim
+			)
+		)
+		left += 4
+	return barCSS
 
-    return barCSS
 
 def loadImageB64(url):
-    resposne = requests.get(url)
-    return b64encode(resposne.content).decode("ascii")
+	resposne = requests.get(url)
+	return b64encode(resposne.content).decode("ascii")
+
 
 def makeSVG(data):
-    barCount = 84
-    contentBar = "".join(["<div class='bar'></div>" for i in range(barCount)])
-    barCSS = barGen(barCount)
+	barCount = 84
+	contentBar = "".join(["<div class='bar'></div>" for i in range(barCount)])
+	barCSS = barGen(barCount)
 
-    if data == {} or data["item"] == 'None':
-        #contentBar = "" #Shows/Hides the EQ bar if no song is currently playing
-        currentStatus = "Last seen playing:"
-        recentPlays = recentlyPlayed()
-        recentPlaysLength = len(recentPlays["items"])
-        itemIndex = random.randint(0, recentPlaysLength - 1)
-        item = recentPlays["items"][itemIndex]["track"]
-    else:
-        item = data["item"]
-        currentStatus = "Vibing to:"
+	if data == {} or data["item"] == "None" or data["item"] is None:
+		# contentBar = "" #Shows/Hides the EQ bar if no song is currently playing
+		currentStatus = "Was playing:"
+		recentPlays = recentlyPlayed()
+		recentPlaysLength = len(recentPlays["items"])
+		itemIndex = random.randint(0, recentPlaysLength - 1)
+		item = recentPlays["items"][itemIndex]["track"]
+	else:
+		item = data["item"]
+		currentStatus = "Vibing to:"
+	image = loadImageB64(item["album"]["images"][1]["url"])
+	artistName = item["artists"][0]["name"].replace("&", "&amp;")
+	songName = item["name"].replace("&", "&amp;")
 
-    image = loadImageB64(item["album"]["images"][1]["url"])
-    artistName = item["artists"][0]["name"].replace("&", "&amp;")
-    songName = item["name"].replace("&", "&amp;")
+	dataDict = {
+		"contentBar": contentBar,
+		"barCSS": barCSS,
+		"artistName": artistName,
+		"songName": songName,
+		"image": image,
+		"status": currentStatus,
+	}
 
-    dataDict = {
-        "contentBar": contentBar,
-        "barCSS": barCSS,
-        "artistName": artistName,
-        "songName": songName,
-        "image": image,
-        "status": currentStatus
-    }
+	return render_template("spotify.html.j2", **dataDict)
 
-    return render_template("spotify.html.j2", **dataDict)
 
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
 def catch_all(path):
-    data = nowPlaying()
-    svg = makeSVG(data)
+	data = nowPlaying()
+	svg = makeSVG(data)
 
-    resp = Response(svg, mimetype="image/svg+xml")
-    resp.headers["Cache-Control"] = "s-maxage=1"
+	resp = Response(svg, mimetype="image/svg+xml")
+	resp.headers["Cache-Control"] = "s-maxage=1"
 
-    return resp
+	return resp
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
